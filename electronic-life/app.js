41ccd678cb36f49d4c4b1f58614cc516be814fc9
Ryan Moore <ryan.moore@mx.com>
2016-11-17 09:57:02 -0700

    only handling action if in action types, adding kill option

--- electronic-life/app.js
+++ electronic-life/app.js
@@ -208,10 +208,26 @@ World.prototype.toString = function () {
   return output;
 }
 
+var actionTypes = Object.create(null);
+
 function LifelikeWorld (map, legend) {
   World.call(this, map, legend);
 }
 LifelikeWorld.prototype = Object.create(World.prototype)
+LifelikeWorld.prototype.letAct = function (critter, vector) {
+  var action = critter.act(new View(this, vector));
+  var handled = action &&
+                action.type in actionTypes &&
+                actionTypes[action.type].call(this, critter, vector, action);
+
+  if (!handled) {
+    critter.energy -= -0.2;
+    if (critter.energy <= 0) {
+      this.grid.set(vector, null)
+    }
+  }
+}
+
 
 //INSTANTIATE & ANIMATE
 var plan = ["############################",
