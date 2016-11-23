95c556ba6406b932c23df80b5ff33d06f1f5c6e7
Ryan Moore <ryan.moore@mx.com>
2016-11-22 18:47:43 -0700

    plants r growing

--- electronic-life/app.js
+++ electronic-life/app.js
@@ -124,10 +124,32 @@ WallFollower.prototype.act = function (view) {
 
 function Wall () {}
 
+function Plant () {
+  this.energy = 3 + Math.random() * 4;
+}
+Plant.prototype.act = function (view) {
+  if (this.energy > 15) {
+    var space = view.find(' ');
+    if (space) {
+      return {
+        type: 'reproduce',
+        direction: space
+      };
+    }
+  }
+
+  if (this.energy < 20) {
+    return {
+      type: 'grow'
+    };
+  }
+};
+
 var legend = {
   '#': Wall,
   '~': WallFollower,
-  'o': BouncingCritter
+  'o': BouncingCritter,
+  "*": Plant
 };
 
 //WORLD
@@ -210,6 +232,49 @@ World.prototype.toString = function () {
 
 var actionTypes = Object.create(null);
 
+actionTypes.grow = function (critter) {
+  critter.energy += 0.5;
+  return true;
+};
+
+actionTypes.move = function (critter, vector, action) {
+  var destination = this.checkDestination(action, vector);
+
+  if (destination == null || critter.energy <= 1 || this.grid.get(destination) != null) {
+    return false
+  }
+
+  critter.energy -= 1;
+  this.grid.set(vector, null);
+  this.grid.set(destination, critter);
+  return true;
+};
+
+actionTypes.eat = function (critter, vector, action) {
+  var destination = this.checkDestination(action, vector);
+  var atDestination = destination != null && this.grid.get(destination);
+
+  if(!atDestination || atDestination.energy == null) {
+    return false;
+  }
+
+  critter.energy += atDestination.energy;
+  this.grid.set(destination, null);
+  return true;
+};
+
+actionTypes.reproduce = function (critter, vector, action) {
+  var baby  = elementFromCharacter(this.legend, critter.originalCharacter);
+  var destination = this.checkDestination(action, vector);
+
+  if (destination == null || critter.energy <= 2 * baby.energy || this.grid.get(destination) != null) {
+    return false;
+  }
+  critter.energy -= 2 * baby.energy;
+  this.grid.set(destination, baby);
+  return true;
+};
+
 function LifelikeWorld (map, legend) {
   World.call(this, map, legend);
 }
@@ -230,18 +295,18 @@ LifelikeWorld.prototype.letAct = function (critter, vector) {
 
 
 //INSTANTIATE & ANIMATE
-var plan = ["############################",
-            "#~      #    #      o      ##",
-            "#                          #",
-            "#          #####           #",
-            "##         #   #    ##     #",
-            "###           ##     #     #",
-            "#           ###      #     #",
-            "#   ####                   #",
-            "#   ##       o             #",
-            "# o  #         o       ### #",
-            "#    #                     #",
-            "############################"];
+var plan = ['############################',
+            '#~      #    #      o      #',
+            '#    *                     #',
+            '#          #####           #',
+            '##         #   #    ##     #',
+            '###           ##     #     #',
+            '#           ###      #     #',
+            '#   ####                   #',
+            '#   ##       o             #',
+            '# o  #         o       ### #',
+            '#    #                     #',
+            '############################'];
 var world = new LifelikeWorld(plan, legend);
 var pre = document.createElement('pre');
 pre.id = 'container';
